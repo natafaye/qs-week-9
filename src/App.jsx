741ca3e20905a1c0f69a6a0f379eb94553c9e0db
Natalie Childs <nata.faye@gmail.com>
2026-04-17 17:32:24 -0700

    talking to API

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,33 @@ import BookList from "./BookList"
 import { ALL_BOOKS } from "./books-data"
 import GenreSelector from "./GenreSelector"
 import AlternateGenreSelector from "./AlternateGenreSelector"
+import { BOOKS_API_KEY } from "./API_KEYS"
 
 export default function App() {
   const [searchValue, setSearchValue] = useState("")
   //const [genreValue, setGenreValue] = useState("Science Fiction")
   const [genreValue, setGenreValue] = useState(["Fiction", "Science Fiction"])
+  const [matchingBooks, setMatchingBooks] = useState([])
+
+  const getGoogleBooks = async () => {
+    // Making an HTTP request with particular URL parameters and waiting for the response
+    const response = await fetch("https://www.googleapis.com/books/v1/volumes?q=" + searchValue + "&key=" + BOOKS_API_KEY)
+    // Unsmooshing the data out of JSON and into normal JS objects and arrays
+    const data = await response.json()
+    // Put the data in state, which triggers a re-render, which means it shows up in the page
+    setMatchingBooks(data.items)
+  }
 
   // Give me the books that match the search term in the textbox
   // Genre matches and either title or author or description
-  const matchingBooks = ALL_BOOKS.filter(book =>
-    genreValue.includes(book.genre) && 
-    (
-      book.title.toLowerCase().includes(searchValue.toLowerCase()) ||
-      book.author.toLowerCase().includes(searchValue.toLowerCase()) ||
-      book.description.toLowerCase().includes(searchValue.toLowerCase())
-    )
-  )
+  // const matchingBooks = ALL_BOOKS.filter(book =>
+  //   genreValue.includes(book.genre) && 
+  //   (
+  //     book.title.toLowerCase().includes(searchValue.toLowerCase()) ||
+  //     book.author.toLowerCase().includes(searchValue.toLowerCase()) ||
+  //     book.description.toLowerCase().includes(searchValue.toLowerCase())
+  //   )
+  // )
 
   return (
     <div>
@@ -31,6 +42,7 @@ export default function App() {
       />
       <button
         className="bg-green-800 text-white rounded-2xl p-2 border-green-900 border rounded-s-none hover:bg-green-700"
+        onClick={getGoogleBooks}
       >
         Search
       </button>
--- src/BookList.jsx
+++ src/BookList.jsx
@@ -3,10 +3,9 @@ export default function BookList({ books }) {
         <div>
             { books.map(book => (
                 <div className="p-3 m-3 border-b border-gray-400">
-                    <h3 className="text-xl">{book.title}</h3>
-                    <h4 className="text-lg italic">{book.author}</h4>
-                    <h4>{book.genre}</h4>
-                    <p className="pt-1">{book.description}</p>
+                    <h3 className="text-xl">{book.volumeInfo.title}</h3>
+                    <h4 className="text-lg italic">{book.volumeInfo.authors}</h4>
+                    <h4>{book.volumeInfo.categories}</h4>
                 </div>
             ))}
         </div>
